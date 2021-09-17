42d4a4ce109dd4c835ba7db3174a08c63045b8d8
A.J.Sarmah <thisisamlan49@gmail.com>
2021-09-17 21:34:15 +0530

    Added multiple conversion feature in formatConvertor.js

--- src/formatConvertor.js
+++ src/formatConvertor.js
@@ -1,15 +1,53 @@
-const arrayUp = (arr)=>{
-    return parseInt(arr.join(''));
-}
+const arrayUp = (arr) => {
+  return arr.join("").toString();
+};
 
-const decimalToBinaryConvertor = (number)=>{
-    let decimalDigitsAsArrays = []
-    while(number > 0){
-        decimalDigitsAsArrays.push(number % 2)
-        number = Math.floor(number / 2)
+const decimalToBinaryConvertor = (number) => {
+  let decimalDigitsAsArrays = [];
+  base = 2;
+  while (number > 0) {
+    decimalDigitsAsArrays.push(number % base);
+    number = Math.floor(number / base);
+  }
+  decimalDigitsAsArrays = decimalDigitsAsArrays.reverse();
+  return arrayUp(decimalDigitsAsArrays);
+};
+
+const decimalToHexConvertor = (number) => {
+  const hexDigits = [];
+  base = 16;
+  while (number != 0) {
+    hexDigits.push(number % base);
+    number = Math.floor(number / base);
+  }
+  for (let i = 0; i < hexDigits.length; i++) {
+    if (hexDigits[i] == 10) {
+      hexDigits[i] = "A";
+    } else if (hexDigits[i] == 11) {
+      hexDigits[i] = "B";
+    } else if (hexDigits[i] == 12) {
+      hexDigits[i] = "C";
+    } else if (hexDigits[i] == 13) {
+      hexDigits[i] = "D";
+    } else if (hexDigits[i] == 14) {
+      hexDigits[i] = "E";
+    } else if (hexDigits[i] == 15) {
+      hexDigits[i] = "F";
     }
-    decimalDigitsAsArrays =  decimalDigitsAsArrays.reverse()
-    return arrayUp(decimalDigitsAsArrays)
-}
+  }
+  hexDigits.reverse();
+  return arrayUp(hexDigits);
+};
 
-console.log(decimalToBinaryConvertor(24569))
+const decimalToOctalConvertor = (number) => {
+  const octalDigits = [];
+  base = 8;
+  while (number != 0) {
+    octalDigits.push(number % base);
+    number = Math.floor(number / base);
+  }
+  octalDigits.reverse();
+  return arrayUp(octalDigits);
+};
+
+console.log(decimalToOctalConvertor(345678));
