54d93b43203d63b7f36270401c7e60e6e705a434
A.J.Sarmah <thisisamlan49@gmail.com>
2021-09-18 10:06:15 +0530

    Basic reducer impleamentation

--- src/reducer.js
+++ src/reducer.js
@@ -1,37 +1,50 @@
-import {combineReducers} from "redux";
+import { combineReducers } from "redux";
+import {
+  decimalToBinaryConvertor,
+  decimalToHexConverotr,
+  decimalToOctalConvertor,
+  hexToDecimalConvertor,
+  binaryToDecimalConvertor,
+  octalToDecimalConvertor,
+} from "./formatConvertor";
 
-const calculations = (state=0,action) => {
-    switch (action.type){
-        case "CHANGE":
-            return action.number
-        case "NULL":
-            return 0
-        case "DECIMAL_TO_HEX":
-            return 0
-        case "DECIMAL_TO_BINARY":
-            return 0
-        case "DECIMAL_TO_OCTAL":
-            return 0
-        case "HEX_TO_DECIMAL":
-            return 0
-        case "BINARY_TO_DECIMAL":
-            return 0
-        case "OCTAL_TO_DECIMAL":
-            return 0
-        default:
-            return 0
-    }
-}
+const calculations = (state = "0", action) => {
+  switch (action.type) {
+    case "CHANGE":
+      return action.number;
+    case "NULL":
+      return 0;
+    case "DECIMAL_TO_HEX":
+      return decimalToHexConverotr(parseInt(action.number));
+    case "DECIMAL_TO_BINARY":
+      return decimalToBinaryConvertor(parseInt(action.number));
+    case "DECIMAL_TO_OCTAL":
+      return decimalToOctalConvertor(parseInt(action.number));
+    case "HEX_TO_DECIMAL":
+      return hexToDecimalConvertor(action.number);
+    case "BINARY_TO_DECIMAL":
+      return binaryToDecimalConvertor(action.number);
+    case "OCTAL_TO_DECIMAL":
+      return octalToDecimalConvertor(action.number);
+    default:
+      return 0;
+  }
+};
 
-const format = (state={from:"DECIMAL",to:"BINARY"},action)=>{
-    switch(action.type){
-        case "CHANGE_IN_FORMAT_FORM":
-            return {...state,from:action.from}
-        case "CHANGE_IN_FORMAT_TO":
-            return {...state,from:action.to}
-        default:
-            return {...state}
-    }
-}
+const format = (
+  state = { from: "DECIMAL", to: "BINARY", isValid: true },
+  action
+) => {
+  switch (action.type) {
+    case "CHANGE_IN_FORMAT_FORM":
+      return { ...state, from: action.from };
+    case "CHANGE_IN_FORMAT_TO":
+      return { ...state, to: action.to };
+    case "CHANGE_IN_VALIDITY":
+      return { ...state, isValid: !isValid };
+    default:
+      return { ...state };
+  }
+};
 
-export const converter = combineReducers({calculations,format})
+export const converter = combineReducers({ calculations, format });
