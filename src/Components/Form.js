71a028b63430420d226210c0bfbc8f6fd1021a1d
A.J.Sarmah <thisisamlan49@gmail.com>
2021-09-26 12:19:52 +0530

    Modified Change format and tweaked executeConversion method to provide floating point support in execution .

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -6,8 +6,16 @@ export let Form = ({ state, dispatch }) => {
   let value;
   const changeFormat = (field) => {
     if (from.value === to.value) {
-      from.value = "decimal";
-      to.value = "binary";
+      let array = ["decimal", "binary", "octal", "hex"];
+      if (state.format.from === to.value) {
+        to.value = state.format.from;
+        array = array.filter((element) => element !== state.format.from);
+        from.value = array[Math.floor(Math.random() * array.length)];
+      } else if (state.format.to === from.value) {
+        from.value = state.format.to;
+        array = array.filter((element) => element !== state.format.to);
+        to.value = array[Math.floor(Math.random() * array.length)];
+      }
     }
     if (field === "from") {
       dispatch({ type: "CHANGE_IN_FORMAT_FORM", from: from.value });
@@ -17,85 +25,143 @@ export let Form = ({ state, dispatch }) => {
   };
   const executeConversion = (number) => {
     const arrayOfTheNumber = number.split("");
-    let isValid = true
+    let isValid = true;
     //for binary
     if (state.format.from === "binary") {
-      arrayOfTheNumber.forEach((num) => {
-        if (Number(num) !== 0 && Number(num) !== 1) {
-          value.value = "";
-          isValid = false
-        }
-      });
-      if(isValid){
-        if(state.format.to === "decimal"){
-          dispatch({type:"BINARY_TO_DECIMAL", number:String(number)})
-        }
-        else if(state.format.to === "octal"){
-          dispatch({type:"BINARY_TO_OCTAL",number:String(number)})
-        }
-        else if(state.format.to === "hex"){
-          dispatch({type:"BINARY_TO_HEX",number:String(number)})
+      if (state.format.to === "decimal") {
+        arrayOfTheNumber.forEach((num) => {
+          if (Number(num) !== 0 && Number(num) !== 1 && num !== ".") {
+            value.value = "";
+            isValid = false;
+          }
+        });
+      } else {
+        arrayOfTheNumber.forEach((num) => {
+          if (Number(num) !== 0 && Number(num) !== 1) {
+            value.value = "";
+            isValid = false;
+          }
+        });
+      }
+      if (isValid) {
+        if (state.format.to === "decimal") {
+          dispatch({ type: "BINARY_TO_DECIMAL", number: String(number) });
+        } else if (state.format.to === "octal") {
+          dispatch({ type: "BINARY_TO_OCTAL", number: String(number) });
+        } else if (state.format.to === "hex") {
+          dispatch({ type: "BINARY_TO_HEX", number: String(number) });
         }
       }
     }
     //for decimal
-    if(state.format.from === "decimal"){
-      arrayOfTheNumber.forEach((num) => {
-        if (Number(num) !== 0 && Number(num) !== 1 && Number(num) !==2 && Number(num) !== 3 && Number(num) !== 4 && Number(num) !== 5 && Number(num) !== 6 && Number(num) !== 7 && Number(num) !== 8 && Number(num)!== 9) {
-          value.value = "";
-          isValid = false
-          console.log(Number(num))
-        }
-      });
-      if(isValid){
-        if(state.format.to === "binary"){
-          dispatch({type:"DECIMAL_TO_BINARY", number:String(number)})
-        }
-        else if(state.format.to === "octal"){
-          dispatch({type:"DECIMAL_TO_OCTAL",number:String(number)})
-        }
-        else if(state.format.to === "hex"){
-          dispatch({type:"DECIMAL_TO_HEX",number:String(number)})
+    if (state.format.from === "decimal") {
+      if (state.format.to === "binary") {
+        arrayOfTheNumber.forEach((num) => {
+          if (
+            Number(num) !== 0 &&
+            Number(num) !== 1 &&
+            Number(num) !== 2 &&
+            Number(num) !== 3 &&
+            Number(num) !== 4 &&
+            Number(num) !== 5 &&
+            Number(num) !== 6 &&
+            Number(num) !== 7 &&
+            Number(num) !== 8 &&
+            Number(num) !== 9 &&
+            num !== "."
+          ) {
+            value.value = "";
+            isValid = false;
+          }
+        });
+      } else {
+        arrayOfTheNumber.forEach((num) => {
+          if (
+            Number(num) !== 0 &&
+            Number(num) !== 1 &&
+            Number(num) !== 2 &&
+            Number(num) !== 3 &&
+            Number(num) !== 4 &&
+            Number(num) !== 5 &&
+            Number(num) !== 6 &&
+            Number(num) !== 7 &&
+            Number(num) !== 8 &&
+            Number(num) !== 9
+          ) {
+            value.value = "";
+            isValid = false;
+          }
+        });
+      }
+      if (isValid) {
+        if (state.format.to === "binary") {
+          dispatch({ type: "DECIMAL_TO_BINARY", number: String(number) });
+        } else if (state.format.to === "octal") {
+          dispatch({ type: "DECIMAL_TO_OCTAL", number: String(number) });
+        } else if (state.format.to === "hex") {
+          dispatch({ type: "DECIMAL_TO_HEX", number: String(number) });
         }
       }
     }
     //for octal
-    if(state.format.from === "octal"){
+    if (state.format.from === "octal") {
       arrayOfTheNumber.forEach((num) => {
-        if (Number(num) !== 0 && Number(num) !== 1 && Number(num) !==2 && Number(num) !== 3 && Number(num) !== 4 && Number(num) !== 5 && Number(num) !== 6 && Number(num) !== 7 ) {
+        if (
+          Number(num) !== 0 &&
+          Number(num) !== 1 &&
+          Number(num) !== 2 &&
+          Number(num) !== 3 &&
+          Number(num) !== 4 &&
+          Number(num) !== 5 &&
+          Number(num) !== 6 &&
+          Number(num) !== 7
+        ) {
           value.value = "";
-          isValid = false
+          isValid = false;
         }
       });
-      if(isValid){
-        if(state.format.to === "binary"){
-          dispatch({type:"OCTAL_TO_BINARY", number:String(number)})
-        }
-        else if(state.format.to === "decimal"){
-          dispatch({type:"OCTAL_TO_DECIMAL",number:String(number)})
-        }
-        else if(state.format.to === "hex"){
-          dispatch({type:"OCTAL_TO_HEX",number:String(number)})
+      if (isValid) {
+        if (state.format.to === "binary") {
+          dispatch({ type: "OCTAL_TO_BINARY", number: String(number) });
+        } else if (state.format.to === "decimal") {
+          dispatch({ type: "OCTAL_TO_DECIMAL", number: String(number) });
+        } else if (state.format.to === "hex") {
+          dispatch({ type: "OCTAL_TO_HEX", number: String(number) });
         }
       }
     }
     // for hex
-    if(state.format.from === "hex"){
+    if (state.format.from === "hex") {
       arrayOfTheNumber.forEach((num) => {
-        if (Number(num) !== 0 && Number(num) !== 1 && Number(num) !==2 && Number(num) !== 3 && Number(num) !== 4 && Number(num) !== 5 && Number(num) !== 6 && Number(num) !== 7 && Number(num) !== 8 && Number(num) !== 9 && num !== "A" && num !=="B" && num !== "C" && num !== "D" && num!== "E" && num !=="F") {
+        if (
+          Number(num) !== 0 &&
+          Number(num) !== 1 &&
+          Number(num) !== 2 &&
+          Number(num) !== 3 &&
+          Number(num) !== 4 &&
+          Number(num) !== 5 &&
+          Number(num) !== 6 &&
+          Number(num) !== 7 &&
+          Number(num) !== 8 &&
+          Number(num) !== 9 &&
+          num !== "A" &&
+          num !== "B" &&
+          num !== "C" &&
+          num !== "D" &&
+          num !== "E" &&
+          num !== "F"
+        ) {
           value.value = "";
-          isValid = false
+          isValid = false;
         }
       });
-      if(isValid){
-        if(state.format.to === "binary"){
-          dispatch({type:"HEX_TO_BINARY", number:String(number)})
-        }
-        else if(state.format.to === "decimal"){
-          dispatch({type:"HEX_TO_DECIMAL",number:String(number)})
-        }
-        else if(state.format.to === "octal"){
-          dispatch({type:"HEX_TO_OCTAL",number:String(number)})
+      if (isValid) {
+        if (state.format.to === "binary") {
+          dispatch({ type: "HEX_TO_BINARY", number: String(number) });
+        } else if (state.format.to === "decimal") {
+          dispatch({ type: "HEX_TO_DECIMAL", number: String(number) });
+        } else if (state.format.to === "octal") {
+          dispatch({ type: "HEX_TO_OCTAL", number: String(number) });
         }
       }
     }
@@ -117,9 +183,14 @@ export let Form = ({ state, dispatch }) => {
           />
         </div>
         <div className="col-xl-4 col-6 d-flex justify-content-center">
-          <button className="btn btn-warning py-0 mt-3" onClick={()=>{
-            executeConversion(value.value)
-          }}>Calculate</button>
+          <button
+            className="btn btn-warning py-0 mt-3"
+            onClick={() => {
+              executeConversion(value.value);
+            }}
+          >
+            Calculate
+          </button>
         </div>
       </div>
       <div className="row justify-content-center mb-5">
